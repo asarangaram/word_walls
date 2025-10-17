8c3d2365b5b4c3a8ea276035b2b680574d6f6dac
Ananda Sarangaram <4833102+asarangaram@users.noreply.github.com>
2025-10-17 19:33:50 +0530

    warnings

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:word_walls/views/app.dart';
-import 'package:word_walls/listeners/nav_listener.dart';
-import 'package:word_walls/notifiers/navigate.dart';
-import 'package:word_walls/views/page_home.dart';
-import 'package:word_walls/views/page_pref.dart';
 
 void main() {
   runApp(const App());
